10096150111b8a9d6a7ade93ed0fc66a5afff022
ShiyaoSun123 <119257562+ShiyaoSun123@users.noreply.github.com>
2022-12-18 17:55:01 -0500

    sa

--- solver_1Dbar.py
+++ solver_1Dbar.py
@@ -1,13 +1,10 @@
 import numpy as np
 from numpy.linalg import inv
-from numpy.linalg import det
 
 from mesh_gen import mesh_gen
 from ke_fun import ke_fun
-#from shape_fun import shape_fun
 from pe_fun import pe_fun
 from slice_fun import slice_fun
-#from gauss_1d import gauss_1d
 
 #u_supp is prescribed displacement at support
 #supp_dof is the support dof
@@ -32,16 +29,18 @@ def solver_1Dbar(nele, nipt, L, E, A, supp_dof,u_supp, ID, taue):
         pe = pe_fun(taue, nen, xe, nipt)
 
         P[LM[:, i]] = pe + P[LM[:, i]]
-        Pf = P[free_dof]
+        Pf = P[free_dof].view()
+
 
         #K[slice_fun(LM[:, i], LM[:, i])] = ke + K[slice_fun(LM[:, i], LM[:, i])]
         K[np.ix_(LM[:, i], LM[:, i])] = ke + K[np.ix_(LM[:, i], LM[:, i])]
 
-        Kff = K[np.ix_(free_dof, free_dof)]
 
-        Kss = K[np.ix_(supp_dof, supp_dof)]
-        Kfs = K[np.ix_(free_dof, supp_dof)]
-        Ksf = K[np.ix_(supp_dof, free_dof)]
+    Kff = K[np.ix_(free_dof, free_dof)].view()
+    Kss = K[np.ix_(supp_dof, supp_dof)].view()
+    Kfs = K[np.ix_(free_dof, supp_dof)].view()
+    Ksf = K[np.ix_(supp_dof, free_dof)].view()
+
 
     Uf = inv(Kff) @ (Pf - Kss @ u_supp)
 
@@ -59,9 +58,6 @@ A = 1
 
 supp_dof = np.array([0])
 u_supp = np.array([0])
-
-
-
 
 
 
